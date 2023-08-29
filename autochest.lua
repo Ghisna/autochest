99ec9b872566ac040d85998d460ebc18c0599758
Ghisna <100676097+Ghisna@users.noreply.github.com>
2023-08-29 23:25:05 +0700

    Add files via upload

---
+++ autochest.lua
@@ -0,0 +1,21 @@
+spawn(function() 
+    repeat
+        task.wait()
+    until game:IsLoaded()
+    repeat
+        task.wait()
+    until game.Players
+    repeat
+        task.wait()
+    until game.Players.LocalPlayer and game.Players.LocalPlayer.Team ~= nil 
+    wait(1.5)
+    require(game.ReplicatedStorage.Notification).new("<Color=Cyan>To Everyone Who Want Use This Hub!<Color=/>"):Display()
+    require(game.ReplicatedStorage.Notification).new("<Color=Orange>If You Wanna Script Update Please:!<Color=/>"):Display()
+    require(game.ReplicatedStorage.Notification).new("<Color=Purple>Donate to: MOMO/CARDVIP/TSR: 0395277909 PHAN THI DON/VO MINH HUNG!<Color=/>"):Display()
+    require(game.ReplicatedStorage.Notification).new("<Color=Red>If enough 400K VND (20$) I'll Update More Haha: 100/400<Color=/>"):Display()
+end)
+spawn(function()
+    loadstring(game:HttpGet(
+        "https://raw.githubusercontent.com/LumosSera/SeraHub/main/che.lua"
+    ))()
+end)
