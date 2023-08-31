b302d1206586d0dd9ea6d07b7627ba33b3e02640
Ghisna <100676097+Ghisna@users.noreply.github.com>
2023-08-31 23:07:22 +0700

    autochest.lua

--- autochest.lua
+++ autochest.lua
@@ -1,4 +1,8 @@
-
+getgenv().Config = {
+    ["Select Team"] = "Pirates", -- Marines/Pirates
+    ["White Screen"] = false,
+    ["Stop After Have God Chalice Or Fist Of Darkness"] = true
+}
 spawn(function() 
     repeat
         task.wait()
@@ -9,15 +13,9 @@ spawn(function()
     repeat
         task.wait()
     until game.Players.LocalPlayer and game.Players.LocalPlayer.Team ~= nil 
-    wait(1.5)
-    require(game.ReplicatedStorage.Notification).new("<Color=Cyan>To Everyone Who Want Use This Hub!<Color=/>"):Display()
 spawn(function()
     loadstring(game:HttpGet(
         "https://raw.githubusercontent.com/LumosSera/SeraHub/main/che.lua"
     ))()
 end)
-getgenv().Config = {
-    ["Select Team"] = "Pirates", -- Marines/Pirates
-    ["White Screen"] = false,
-    ["Stop After Have God Chalice Or Fist Of Darkness"] = true
-}
+
